73f1d337e872853fde5afae00d0972da6ca0c567
arnaau96 <arnau_sarabia@hotmail.com>
2017-04-03 21:31:46 +0200

    ENTREGA FASE 2

--- routes/tareas.js
+++ routes/tareas.js
@@ -4,25 +4,36 @@ var vartareas = [
     {
         "nom":"Diseño wireframe",
         "etiquetes": [ "wireframe","app","hibrida"],
-        "realització":"45"
+        "realització":"25"
     },
     {
         "nom":"Reunión",
         "etiquetes": ["app","hibrida"],
-        "realització":"20"
+        "realització":"50"
     },
     {
         "nom":"Desarrollo Layout",
         "etiquetes": [ "layout","app","hibrida"],
-        "realització":"58"
+        "realització":"75"
     },
     {
         "nom":"Reunión cliente",
         "etiquetes": [ "reunión","app","hibrida"],
-        "realització":"440"
+        "realització":"100"
     }
 ]
 router.get('/', function(req, res, next) {
     res.render('tareas', {title: 'Tareas', tareas:vartareas});
+});
+router.post('/crear', function(req,res,next) {
+    var arrTags = req.body.etiquetes;
+    arrTags = arrTags.split(',');
+    var nuevaTarea = {
+        nom: req.body.nom,
+        etiquetes: arrTags,
+        realització: req.body.realit
+    }
+    vartareas.push(nuevaTarea);
+    res.redirect('/tareas');
 });
 module.exports = router;
